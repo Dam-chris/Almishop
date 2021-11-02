ca130d9c9969bdee4938fa01b9d4eb00f900fdc5
christian <crojasantillan@gmail.com>
2021-11-02 11:50:51 +0100

    data tables aplicado en products y data-tables-config implementado

---
+++ src/app/components/data-tables-config/language_ES.ts
@@ -0,0 +1,17 @@
+export class Language_ES 
+{
+    processing: "Procesando...";
+    lengthMenu: "Mostrar _MENU_ registros";
+    zeroRecords: "No se encontraron resultados";
+    emptyTable: "Ningún dato disponible en esta tabla";
+    infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros";
+    infoFiltered: "(filtrado de un total de _MAX_ registros)";
+    search: "Buscar:";
+    loadingRecords: "Cargando...";
+    paginate: {
+        "first": "Primero",
+        "last": "Último",
+        "next": "Siguiente",
+        "previous": "Anterior"
+    }
+}
