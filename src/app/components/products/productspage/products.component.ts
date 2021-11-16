f2629ab16c758fca28e78f63ad6b9e69cbf5854b
Dam-chris <crojasantillan@gmail.com>
2021-11-16 16:31:46 +0100

    antes del merge con remote

--- src/app/components/products/productspage/products.component.ts
+++ src/app/components/products/productspage/products.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from '../services/product.service'; 
 import { Language_ES } from '../../data-tables-config/language_ES'; 
 import swal from 'sweetalert';
+import Swal from 'sweetalert2';
+import { Smartphone } from '../models/smartphone';
 
 
 @Component({
@@ -58,5 +60,43 @@ export class ProductsComponent implements OnInit
 
   archiveProduct(idProduct: number) {
     console.log('Archived: ' + idProduct)
+  }
+
+  selectType()
+  {
+  
+    const inputOptions = {
+          smartphone: 'smartphone',
+          tablet: 'tablet',
+          videoconsole: 'consola',
+          videogame: 'videojuego'
+        };
+
+    
+    Swal.fire({
+      title: '¿Selecciona que producto quieres crear?',
+      input: 'radio',
+      inputOptions: inputOptions,
+      confirmButtonColor: '#0275d8',
+      inputValidator:(res) => 
+      { 
+        console.log(res);
+        if(res)
+        {
+          this.router.navigate(['/products/add', {type: res}]);
+          Swal.close();
+        }
+       return 'Debes seleccionar uno!!' 
+      }
+
+    });
+    
+    /*   
+      recibirlo
+      constructor(private router: Router) {
+      console.log(this.router.getCurrentNavigation().extras.state.example); // should log out 'bar'
+    }
+  */
+    
   }
 }
